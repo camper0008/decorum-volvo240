5aa959dc460f940cdb11010f6760d4f3cd4d8f5e
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2023-10-26 15:15:53 +0200

    elaborate on contact page

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -6,8 +6,8 @@ pub fn contact_page() -> Html {
         <div class="contact">
             <h1>{"Kontakt"}</h1>
             <ul>
-                <li><p>{"Kontakt <email0@example.org>, hvis du har opdaget en fejl, med volvo240."}</p></li>
-                <li><p>{"Kontakt <email1@example.org>, for sponsoring af volvo240.dk m.v."}</p></li>
+                <li><p>{"Kontakt <email0@example.org>, hvis du har opdaget en fejl, med volvo240, du mener der ikke burde opstå."}</p></li>
+                <li><p>{"Kontakt <email1@example.org>, for annoncer på- & sponsoring af- volvo240.dk."}</p></li>
             </ul>
         </div>
     }
